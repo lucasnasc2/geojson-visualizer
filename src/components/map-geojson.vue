03b5aaf403210c0390f6b60cce547cbbd944c5f0
lucasnasc2 <lucas.nasc2.ln@gmail.com>
2023-01-30 01:28:44 +0100

    marker toggle, dates, data list, bug fixes

--- src/components/map-geojson.vue
+++ src/components/map-geojson.vue
@@ -17,12 +17,20 @@
       <database-marker-outline size="36"></database-marker-outline>
     </button>
     <button
-      :disabled="this.locating"
+      :disabled="this.bool.locating"
       @click="this.locate()"
-      class="disabled leaflet-borders p-2 bg-white hover"
+      class="disabled leaflet-borders p-2 mb-2 bg-white hover"
     >
-      <crosshairs v-if="this.locating" size="30"></crosshairs>
+      <crosshairs v-if="this.bool.locating" size="30"></crosshairs>
       <crosshairs-gps v-else size="30"></crosshairs-gps>
+    </button>
+    <button
+      :disabled="!this.geojson"
+      @click="this.toggleAllMarkers()"
+      class="disabled leaflet-borders p-2 bg-white hover"
+      :class="{ disabled: !this.bool.allMarkers }"
+    >
+      <map-marker-multiple-outline size="30"></map-marker-multiple-outline>
     </button>
   </div>
 
@@ -45,7 +53,7 @@
       <upload-outline size="36"></upload-outline>
     </button>
   </div>
-  <div v-if="infoPopup" class="no-scrollbar popup bg-white">
+  <div v-if="this.bool.infoPopup" class="no-scrollbar popup bg-white">
     <div class="popup-top-bar flex content-between">
       <span class="flex-grow self-center pl-2 text-2xl">{{
         geojson.features.length
@@ -57,6 +65,7 @@
     </div>
     <div class="pt-9">
       <div
+        :class="{ 'bg-gray-100': index != this.rangeValue }"
         class="flex flex-row w-full px-4 py-2"
         v-for="(feature, index) in geojson.features"
         :key="index"
@@ -85,18 +94,44 @@ import crosshairsGps from "./icons/crosshairsGps.vue";
 import crosshairs from "./icons/crosshairs.vue";
 import close from "./icons/close.vue";
 import cloneDeep from "lodash/cloneDeep";
+import mapMarkerMultipleOutline from "./icons/MapMarkerMultipleOutline.vue";
 let map;
 let osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   minZoom: 2,
   maxZoom: 19,
   attribution: "© OpenStreetMap",
 });
-let icon = L.divIcon({
+let icon_marker = L.divIcon({
   className: "custom-div-icon",
   html: "<div class='marker-pin'></div>",
+  iconSize: [10, 10],
+  iconAnchor: [5, 5],
+});
+let icon_dot = L.divIcon({
+  className: "custom-div-icon",
+  html: "<div class='marker-dot'></div>",
+  iconSize: [6, 6],
+  iconAnchor: [3, 3],
+});
+let icon_path_start = L.divIcon({
+  className: "custom-div-icon",
+  html: "<div class='marker-pin-start'></div>",
   iconSize: [16, 16],
   iconAnchor: [8, 8],
 });
+let icon_path_end = L.divIcon({
+  className: "custom-div-icon",
+  html: "<div class='marker-pin-end'></div>",
+  iconSize: [16, 16],
+  iconAnchor: [8, 8],
+});
+
+let polyline = null;
+let markers = [];
+let marker = null;
+let markerStart = null;
+let markerEnd = null;
+let locationCircle = null;
 
 export default {
   components: {
@@ -105,31 +140,39 @@ export default {
     crosshairsGps,
     crosshairs,
     close,
+    mapMarkerMultipleOutline,
   },
   data() {
     return {
+      originalGeojson: null,
       geojson: null,
       coordinates: null,
       rangeValue: 0,
-      markers: [],
-      locationCircle: null,
-      locating: false,
-      infoPopup: false,
+      filterDistance: 5,
+      bool: {
+        locating: false,
+        infoPopup: false,
+        allMarkers: false,
+      },
     };
   },
   watch: {
-    geojson(newData) {
-      if (newData) {
-        this.addGeojsonLayers();
+    geojson(newData, oldData) {
+      if (oldData) {
+        this.removeLayers();
       }
+      if (newData) {
+        this.addLayers();
+      }
+      this.rangeValue = 0;
     },
     rangeValue(newData) {
       let i = newData;
-      this.markers[i].openPopup();
+      let coord = this.coordinates[i];
+      this.changeMarker(coord, i);
       map.panTo(this.coordinates[i]);
-      if (this.infoPopup) {
-        const element = document.getElementById("item" + i);
-        element.scrollIntoView({ behavior: "smooth", block: "center" });
+      if (this.bool.infoPopup) {
+        this.scrollToItem(i);
       }
     },
   },
@@ -142,57 +185,115 @@ export default {
   },
   methods: {
     locate() {
-      this.locating = true;
+      this.bool.locating = true;
       map.locate({ setView: true, maxZoom: 16 });
       const self = this;
       function onLocationFound(e) {
-        self.locating = false;
-        self.getCircle(e);
+        self.bool.locating = false;
+        self.addCircle(e);
       }
       map.on("locationfound", onLocationFound);
 
       function onLocationError(e) {
-        self.locating = false;
+        self.bool.locating = false;
         alert(e.message);
       }
       map.on("locationerror", onLocationError);
     },
-    getCircle(e) {
-      if (this.locationCircle) {
-        this.locationCircle.remove();
-        this.locationCircle = null;
+    addCircle(e) {
+      if (locationCircle) {
+        this.removeCircle();
       }
       let radius = e.accuracy;
-      this.locationCircle = L.circle(e.latlng, radius)
+      locationCircle = L.circle(e.latlng, radius)
         .addTo(map)
         .bindPopup("You are within this " + radius + " meters radius");
     },
-    getLine() {
-      let polyline = L.polyline(this.coordinates, {
+    removeCircle() {
+      locationCircle.remove();
+      locationCircle = null;
+    },
+    addPath() {
+      polyline = L.polyline(this.coordinates, {
         weight: 7,
         smoothFactor: 3,
       }).addTo(map);
       map.fitBounds(polyline.getBounds());
-      return polyline;
     },
-    getMarkers() {
-      let markers = [];
-      this.coordinates.forEach((coord, i) => {
-        let marker = L.marker(coord, { icon: icon }).addTo(map);
-        let popupInfo = `${this.geojson.features[i].properties.time}<br>
-        ${coord}`;
-        markers.push(
-          i == 0
-            ? marker.bindPopup(popupInfo).openPopup()
-            : marker.bindPopup(popupInfo)
-        );
+    removePath() {
+      polyline.remove();
+      polyline = null;
+    },
+    getPopupInfo(i) {
+      let tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
+      let dateTime = new Date(this.geojson.features[i].properties.time_long);
+      let date = dateTime.toLocaleDateString("en-US", {
+        timeZone: tz,
       });
-      this.markers = markers;
-      return markers;
+      let time = dateTime.toLocaleTimeString("it-IT", {
+        timeZone: tz,
+      });
+      return `${time}<br/>${date}`;
     },
-    addGeojsonLayers() {
-      this.getMarkers();
-      this.getLine();
+    addStartEndMarkers() {
+      let coordStart = this.coordinates[0];
+      let coordEnd = this.coordinates[this.coordinates.length - 1];
+      markerEnd = L.marker(coordEnd, { icon: icon_path_end }).addTo(map);
+      markerStart = L.marker(coordStart, { icon: icon_path_start }).addTo(map);
+      markerEnd.bindPopup(this.getPopupInfo(this.coordinates.length - 1));
+      markerStart.bindPopup(this.getPopupInfo(0));
+    },
+    removeStartEndMarkers() {
+      markerStart.remove();
+      markerEnd.remove();
+      markerStart = null;
+      markerEnd = null;
+    },
+    changeMarker(coord, i) {
+      if (marker) {
+        marker.remove();
+      }
+      let newMarker = L.marker(coord, { icon: icon_marker }).addTo(map);
+      marker = newMarker.bindPopup(this.getPopupInfo(i)).openPopup();
+    },
+    removeMarker() {
+      if (!marker) return;
+      marker.remove();
+      marker = null;
+    },
+    addAllMarkers() {
+      let arr = [];
+      this.coordinates.forEach((coord, i) => {
+        let marker = L.marker(coord, { icon: icon_dot }).addTo(map);
+        arr.push(marker.bindPopup(this.getPopupInfo(i)));
+      });
+      markers = arr;
+    },
+    removeAllMarkers() {
+      if (markers.length) {
+        markers.forEach((m) => {
+          m.remove();
+        });
+        markers = [];
+      }
+    },
+    toggleAllMarkers() {
+      this.bool.allMarkers = !this.bool.allMarkers;
+      if (this.bool.allMarkers) {
+        this.addAllMarkers();
+      } else {
+        this.removeAllMarkers();
+      }
+    },
+    addLayers() {
+      this.addPath();
+      this.addStartEndMarkers();
+    },
+    removeLayers() {
+      this.removePath();
+      this.removeStartEndMarkers();
+      this.removeAllMarkers();
+      this.removeMarker();
     },
     openFilePicker() {
       this.$refs.fileInput.click();
@@ -208,23 +309,26 @@ export default {
     },
     handleFile(event) {
       let geojson = JSON.parse(event.target.result);
-      let cleaned = this.denoiseGeojson(geojson);
-      this.changeGeojsonData(cleaned);
+      this.changeGeojsonData(geojson);
     },
-    changeGeojsonData(data) {
-      this.geojson = data;
-      let features = cloneDeep(data.features);
+    changeGeojsonData(geojson) {
+      this.originalGeojson = cloneDeep(geojson);
+      let cleaned = this.denoiseGeojson(geojson);
+      this.geojson = cloneDeep(cleaned);
+      let features = cloneDeep(cleaned.features);
       this.coordinates = features.map((feature) =>
         feature.geometry.coordinates.reverse()
       );
     },
-    denoiseGeojson(data, threshold = 5) {
+    denoiseGeojson(data) {
+      let threshold = this.filterDistance;
       let geojson = data;
       let features = cloneDeep(geojson.features);
       let cleanedFeatures = [];
       features.forEach((feature, i) => {
+        let l = features.length - 1;
         let coord = feature.geometry.coordinates;
-        if (i) {
+        if (i && i != l) {
           let oldCoord = features[i - 1].geometry.coordinates;
           let distance = map.distance(oldCoord, coord);
           let valid = distance > threshold;
@@ -239,7 +343,11 @@ export default {
       return geojson;
     },
     toggleInfoPopup() {
-      this.infoPopup = !this.infoPopup;
+      this.bool.infoPopup = !this.bool.infoPopup;
+    },
+    scrollToItem(i) {
+      const element = document.getElementById("item" + i);
+      element.scrollIntoView({ behavior: "smooth", block: "center" });
     },
   },
 };
@@ -254,6 +362,9 @@ export default {
   background-color: #e5e7eb;
 }
 .disabled:disabled {
+  background-color: #d3d3d3;
+}
+.disabled {
   background-color: #d3d3d3;
 }
 .no-scrollbar::-webkit-scrollbar {
@@ -337,8 +448,8 @@ export default {
   background-color: white;
 }
 .marker-pin {
-  width: 16px;
-  height: 16px;
+  width: 10px;
+  height: 10px;
   border-radius: 100%;
   background: white;
   outline: rgba(0, 0, 0, 25%) solid 2px;
@@ -346,11 +457,53 @@ export default {
 }
 .marker-pin::after {
   content: "";
+  width: 4px;
+  height: 4px;
+  margin: 3px 0 0 3px;
+  background: black;
+  position: absolute;
+  border-radius: 50%;
+}
+.marker-pin-start {
+  width: 16px;
+  height: 16px;
+  border-radius: 100%;
+  background: white;
+  outline: rgba(0, 0, 0, 25%) solid 2px;
+  position: absolute;
+}
+.marker-pin-start::after {
+  content: "";
   width: 6px;
   height: 6px;
   margin: 5px 0 0 5px;
   background: black;
   position: absolute;
   border-radius: 50%;
+}
+.marker-pin-end {
+  width: 16px;
+  height: 16px;
+  border-radius: 100%;
+  background: black;
+  outline: rgba(0, 0, 0, 25%) solid 2px;
+  position: absolute;
+}
+.marker-pin-end::after {
+  content: "";
+  width: 6px;
+  height: 6px;
+  margin: 5px 0 0 5px;
+  background: white;
+  position: absolute;
+  border-radius: 50%;
+}
+.marker-dot {
+  width: 6px;
+  height: 6px;
+  border-radius: 100%;
+  background: white;
+  outline: rgba(0, 0, 0, 25%) solid 2px;
+  position: absolute;
 }
 </style>
